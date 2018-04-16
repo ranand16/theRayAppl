bcb4689249b312a76425957f4e6c84c33af7397a
ranand16 <ranand16@gmail.com>
2018-04-17 00:28:45 +0530

    some more bug fixes.

--- theRay0.0/Controller/AttendanceVC.swift
+++ theRay0.0/Controller/AttendanceVC.swift
@@ -18,43 +18,46 @@ class AttendanceVC: UIViewController {
     var classes = [[String]]()
     
     override func viewDidLoad() {
-        noClassesAssigned.numberOfLines = 4
-        noClassesAssigned.sizeToFit()
-        noClassesAssigned.center = self.view.center
         super.viewDidLoad()
+        
         let currentUser = PFUser.current()
-        if(!(currentUser?.value(forKey: "isWhat") as! String).contains("TEACHER") || !(currentUser?.value(forKey: "isWhat") as! String).contains("PRINCIPAL")){
-            print(currentUser?.value(forKey: "isWhat") as! String)
-            noClassesAssigned.isHidden = false // show the messageLabel
-            noClassesAssigned.text = "You dont have permission to take attendance"
-        } else{
+        if((currentUser?.value(forKey: "isWhat") as! String).contains("TEACHER")){
             if(currentUser?.value(forKeyPath: "classAssigned")==nil){
                 noClassesAssigned.text = "You have no classes now"
                 noClassesAssigned.isHidden = false // show the messageLabel
+                noClassesAssigned.numberOfLines = 4
+                noClassesAssigned.sizeToFit()
+                noClassesAssigned.center = self.view.center
             } else{
-                noClassesAssigned.isHidden = true // hide the messageLabel
+                
                 let hr = moment();
-                print(hr.hour)
+                
                 if(hr.hour<7 || hr.hour>15){
                     noClassesAssigned.isHidden = false // show the messageLabel
                     noClassesAssigned.text = "You are only allowed to take attendance in school hours"
+                    noClassesAssigned.numberOfLines = 4
+                    noClassesAssigned.sizeToFit()
+                    noClassesAssigned.center = self.view.center
                 } else{
                     let dayCommence = moment(currentUser?.value(forKey: "dayCommence") as! String)?.get("H");
                     let sessionCommence = moment(currentUser?.value(forKey: "sessionCommence") as! Date)
                     let date = moment();
                     let diffHours = hr.hour - dayCommence!; // say 4th hour
                     let diffDays = date.day - sessionCommence.day; // say 60 days
-                    //            print(diffHours);
-                    //            print(diffDays);
+                    
                     x = diffDays/7 ; // 60/7 = 8 - 1 = 7
                     y = ((diffDays%7)*8) + diffHours; // 4*8 + 4 = 35
-                    //            print(x);
-                    //            print(y);
+                    
                     classes = currentUser?.value(forKey: "classAssigned") as! [[String]];
-                    //            print(classes)
                 }
             }
+        } else{
+            print(currentUser?.value(forKey: "isWhat") as! String)
+            noClassesAssigned.isHidden = false // show the messageLabel
+            noClassesAssigned.text = "You dont have permission to take attendance"
+            noClassesAssigned.numberOfLines = 4
+            noClassesAssigned.sizeToFit()
+            noClassesAssigned.center = self.view.center
         }
-    
     }
 }
