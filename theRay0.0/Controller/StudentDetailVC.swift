bc86afb8b901a2b31dbc6817a0a253818065c93a
ranand16 <ranand16@gmail.com>
2018-05-13 07:39:26 +0530

    Added search results UI

---
+++ theRay0.0/Controller/StudentDetailVC.swift
@@ -0,0 +1,18 @@
+//
+//  StudentDetailVC.swift
+//  theRay0.0
+//
+//  Created by Rishabh Anand on 13/05/18.
+//  Copyright © 2018 Rishabh Anand. All rights reserved.
+//
+
+import UIKit
+
+class StudentDetailVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
